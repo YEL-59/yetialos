524aae84caf4829efbf2b541323f71f9ca6a5284
tofayel <tofayeltuhin143@gmail.com>
2025-09-16 14:45:21 +0600

    add local-flaver

--- src/pages/home/home-components/index.js
+++ src/pages/home/home-components/index.js
@@ -1,2 +1,5 @@
 // Home page components exports
 export { default as HeroSection } from './hero-section'
+export { default as TrendingRestaurants } from './trending-restaurants'
+export { default as NearbyRestaurants } from './nearby-resturants'
+export { default as LocalFlavor } from './local-flavor'
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,13 @@
 
-import { HeroSection } from './home-components'
+import { HeroSection, NearbyRestaurants, TrendingRestaurants, LocalFlavor } from './home-components'
 
 const Home = () => {
     return (
         <div className="space-y-0">
             <HeroSection />
+            <TrendingRestaurants />
+            <LocalFlavor />
+            <NearbyRestaurants />
         </div>
     )
 }
